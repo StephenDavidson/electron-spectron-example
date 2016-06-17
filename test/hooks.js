6a1c74a39d1121e1bfa876891e83987ab98aeeb5
Stephen Davidson <shdavidson90@gmail.com>
2016-06-17 14:56:31 +0200

    add start timeout

--- test/hooks.js
+++ test/hooks.js
@@ -4,17 +4,20 @@ var chaiAsPromised = require('chai-as-promised');
 var electron = require('electron-prebuilt');
 
 global.before(function () {
-    chai.should()
-    chai.use(chaiAsPromised)
+    chai.should();
+    chai.use(chaiAsPromised);
 });
 
 beforeEach(function () {
     this.app = new Application({
+        startTimeout: 10000,
         path: electron,
         args: ['electron-app']
     });
+
     return this.app.start().then(function (app) {
         chaiAsPromised.transferPromiseness = app.transferPromiseness;
+        console.log(app);
         return app
     })
 });
