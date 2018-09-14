04548916de2efe5c71b85f7df62c9ae67095154f
Stephen Davidson <shdavidson90@gmail.com>
2018-09-14 11:20:44 +0200

    trim search page

--- test/page-objects/search.page.js
+++ test/page-objects/search.page.js
@@ -1,10 +1,5 @@
-const searchPage = Object.create({
-  /**
-   * define elements
-   */
+module.exports = {
   searchButton: '#search_button_homepage',
   searchField:  '#search_form_input_homepage',
   searchResult: '.result__a'
-});
-
-module.exports = searchPage;
+};
