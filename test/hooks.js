dc22f104e728101e6182d843ca51b814d0b5af23
Stephen Davidson <shdavidson90@gmail.com>
2018-09-14 10:18:57 +0200

    linting

--- test/hooks.js
+++ test/hooks.js
@@ -4,23 +4,23 @@ const chaiAsPromised = require('chai-as-promised');
 const electron = require('electron');
 
 global.before(() => {
-	chai.should();
-	chai.use(chaiAsPromised);
+  chai.should();
+  chai.use(chaiAsPromised);
 });
 
 module.exports = {
-	async startApp() {
-		const app = await new Application({
-			path: electron,
-			args: ['electron-app']
-		}).start();
-		chaiAsPromised.transferPromiseness = app.transferPromiseness;
-		return app;
-	},
+  async startApp() {
+    const app = await new Application({
+      path: electron,
+      args: ['electron-app']
+    }).start();
+    chaiAsPromised.transferPromiseness = app.transferPromiseness;
+    return app;
+  },
 
-	async stopApp(app) {
-		if (app && app.isRunning()) {
-			await app.stop();
-		}
-	}
+  async stopApp(app) {
+    if (app && app.isRunning()) {
+      await app.stop();
+    }
+  }
 };
