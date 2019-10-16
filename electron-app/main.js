2e139b764ce90a59edcbf3abd016fc0b0cdbfda3
Florin Draghici <draghif@labcorp.com>
2019-10-16 10:31:28 -0400

    upgrade-to-electron-6: Upgrade to Electron 6 (and Spectron 8), increased timeout make app run in nodeIntegration mode explicitly

--- electron-app/main.js
+++ electron-app/main.js
@@ -5,7 +5,8 @@ let mainWindow;
 app.on('ready', () => {
   mainWindow = new BrowserWindow({
     height: 600,
-    width: 800
+    width: 800,
+    webPreferences: {nodeIntegration: true}
   });
 
   mainWindow.loadURL('file://' + __dirname + '/index.html');
